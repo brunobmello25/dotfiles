7bd690a8b4f40d72fabdada2b3a346ad0d72278c
Bruno Mello <bruno.barros.mello@gmail.com>
2021-06-13 16:12:57 -0300

    remove mouse keybindings

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -166,14 +166,6 @@ awful.screen.connect_for_each_screen(function(s)
 end)
 -- }}}
 
--- {{{ Mouse bindings
-root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
-))
--- }}}
-
 -- {{{ Key bindings
 globalkeys = gears.table.join(
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
