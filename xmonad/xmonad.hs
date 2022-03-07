afacf8778ea3859b308abbbcd99fe8373feb7ce4
Bruno Mello <bruno.barros.mello@gmail.com>
2022-03-07 14:44:55 -0300

    colors

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -317,6 +317,7 @@ myLogHook = return ()
 -- myStartupHook = return ()
 myStartupHook :: X ()
 myStartupHook = do
+  spawnOnce "killall xmobar"
   spawnOnce "setxkbmap us -option compose:ralt"
   spawnOnce "nitrogen --restore &"
   spawnOnce "picom &"
