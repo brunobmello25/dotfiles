783a337442092d83eff9afaeb11455db69bb22ca
Bruno Mello <bruno.barros.mello@gmail.com>
2022-03-21 21:09:39 -0300

    neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,26 @@
-require 'settings'
-require 'keymaps'
-require 'plugins.paq'
-require 'colors'
-require 'lsp'
+-- LOAD PLUGINS
+require('plugins')
+
+-- BASIC CONFIG
+local opt = vim.opt
+local cmd = vim.cmd
+
+opt.tabstop = 2
+opt.mouse = 'a'
+opt.expandtab = true
+opt.number = true
+opt.relativenumber = true
+opt.shiftwidth = 2
+opt.scrolloff = 8
+
+cmd('filetype plugin indent on')
+
+-- COLORS CONFIG
+require('catppuccin').setup({
+	transparent_background=true,
+})
+cmd[[colorscheme catppuccin]]
+
+-- SHORTCUTS
+vim.g.mapleader = ' '
+
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,18 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  -- BASIC LUA SETUP
+  use 'wbthomason/packer.nvim'
+
+  -- COLOR THEME
+  use {'catppuccin/nvim', as = 'catppuccin'}
+
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+      }
+    end
+  }
+end)
