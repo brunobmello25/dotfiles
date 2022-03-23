6242c325e47e63f3696142edc9bff435b549ae9e
Bruno Mello <bruno.barros.mello@gmail.com>
2022-03-22 22:00:51 -0300

    vim

---
+++ nvim/lua/basics.lua
@@ -0,0 +1,11 @@
+vim.opt.tabstop = 2
+vim.opt.mouse = 'a'
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 2
+vim.opt.scrolloff = 8
+vim.cmd('filetype plugin indent on')
+
+vim.g.mapleader = ' '
+
