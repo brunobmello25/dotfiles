4589a66fe02e41a8bed2949cc8f20761a1c47340
Bruno Mello <bruno.barros.mello@gmail.com>
2022-04-04 12:39:20 -0300

    plugins comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,28 +9,25 @@ require('signify-config')
 require('treesitter-config')
 
 return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
-  use {'catppuccin/nvim', as = 'catppuccin'}
-  use 'nvim-lua/plenary.nvim'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
+  use 'wbthomason/packer.nvim' -- plugin manager
+  use 'nvim-treesitter/nvim-treesitter' -- syntax highlighting
+  use {'catppuccin/nvim', as = 'catppuccin'} -- color theme
+  use 'nvim-lua/plenary.nvim' -- requirement for other plugins
   use {
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'neovim/nvim-lspconfig'
-  use 'vim-airline/vim-airline'
-  use 'tpope/vim-fugitive'
-  use 'jiangmiao/auto-pairs'
-  use 'tpope/vim-surround'
-  use 'lukas-reineke/lsp-format.nvim'
-  use 'unblevable/quick-scope'
-  use 'ThePrimeagen/vim-be-good'
-  use 'ThePrimeagen/harpoon'
-  use 'mhinz/vim-signify'
-  use 'github/copilot.vim'
+  } -- fuzzy finder
+  use 'hrsh7th/nvim-cmp' -- autocomplete plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- autocomplete plugin
+  use 'neovim/nvim-lspconfig' -- neovim lsp
+  use 'vim-airline/vim-airline' -- statusbar
+  use 'tpope/vim-fugitive' -- git integration
+  use 'jiangmiao/auto-pairs' -- autoclose pairs
+  use 'tpope/vim-surround' -- easy change surrounding tags and quotes
+  use 'lukas-reineke/lsp-format.nvim' -- format on save with lsp
+  use 'unblevable/quick-scope' -- horizontal finder highlight
+  use 'ThePrimeagen/harpoon' -- mark files for navigation
+  use 'mhinz/vim-signify' -- line changes with git
+  use 'github/copilot.vim' -- github copilot
 end)
 
