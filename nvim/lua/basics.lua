bd2e99a12a82acf0ecb3bcb6a84a7ca769f53b9f
Bruno Mello <bruno.barros.mello@gmail.com>
2022-04-08 14:29:19 -0300

    disable vim wrap

--- nvim/lua/basics.lua
+++ nvim/lua/basics.lua
@@ -5,6 +5,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.scrolloff = 8
+vim.opt.wrap = false
 vim.cmd('filetype plugin indent on')
 
 vim.g.mapleader = ' '
