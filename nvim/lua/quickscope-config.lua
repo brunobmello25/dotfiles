6c3ede447cafb804537d783344d08ea3b5d21580
Bruno Mello <bruno.barros.mello@gmail.com>
2022-05-24 13:31:18 -0300

    quickscope config

--- nvim/lua/quickscope-config.lua
+++ nvim/lua/quickscope-config.lua
@@ -1,1 +1,9 @@
-vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
+vim.g.qs_highlight_on_keys = { 'f', 'F', 't', 'T' }
+
+vim.cmd([[
+  augroup qs_colors
+    autocmd!
+    autocmd ColorScheme * highlight QuickScopePrimary guifg='#33ff00' gui=underline ctermfg=155 cterm=underline
+    autocmd ColorScheme * highlight QuickScopeSecondary guifg='#ff00f7' gui=underline ctermfg=81 cterm=underline
+  augroup END
+]])
