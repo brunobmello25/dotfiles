bfc81016e9fffa73437e09057835ec8d61f18a34
Bruno Mello <bruno.barros.mello@gmail.com>
2022-05-25 15:07:27 -0300

    remap vim list todos

--- nvim/lua/todo-config.lua
+++ nvim/lua/todo-config.lua
@@ -1,4 +1,4 @@
 require("todo-comments").setup {
 }
 
-vim.api.nvim_set_keymap('n', '<leader>lt', ':TodoTelescope<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>ft', ':TodoTelescope<CR>', { noremap = true })
