08cb9a484cf16cb50211bb7fef6a325a726a69dc
Bruno Mello <bruno.barros.mello@gmail.com>
2022-05-30 19:20:54 -0300

    add rmi option to find-projects

--- zsh/find-project.sh
+++ zsh/find-project.sh
@@ -34,6 +34,14 @@ fp() {
     echo $current_full_path_name >> $config_file
   elif [[ $# == 1 && $1 == 'rm' ]]; then
     grep -vx ${PWD} $config_file > temp && mv temp $config_file 
+  elif [[ $# == 1 && $1 == 'rmi' ]]; then
+    assert_file_exists $config_dir $config_file
+
+    selected=`grep "" $config_file | fzf`
+
+    grep -vx $selected $config_file > temp && mv temp $config_file 
+
+    echo "Removed \"$selected\" from favorites"
   else
     echo "Invalid arguments. You should either call this script with no arguments to open the projects finder, or call it with \"add\" or \"remove\" arguments"
   fi
