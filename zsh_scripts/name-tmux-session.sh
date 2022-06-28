7856f7fba9b8e6db76f577f5e8b28db9069dcb2a
Bruno Mello <bruno.barros.mello@gmail.com>
2022-06-28 16:20:20 -0300

    change rename-session script

--- zsh_scripts/name-tmux-session.sh
+++ zsh_scripts/name-tmux-session.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+
 if [[ $(echo $TMUX) != "" ]]; then
-  new_name=${PWD##*/}
-  tmux rename-session -t "$(tmux display-message -p "#S")" "${new_name}"
   clear
+
+  printf "Enter a new name for the current session > "
+  read -r
+  new_name=$REPLY
+  tmux rename-session -t "$(tmux display-message -p "#S")" "${new_name}"
+
+  echo "Session renamed to ${new_name}"
+else
+  echo "You must be in a tmux session to rename it."
 fi
 
