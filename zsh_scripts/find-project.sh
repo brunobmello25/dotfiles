1982c13414b0e415d5a2e153e0cb5934921a572a
Bruno Mello <bruno.barros.mello@gmail.com>
2022-07-11 18:22:13 -0300

    add fp clean command to find project

--- zsh_scripts/find-project.sh
+++ zsh_scripts/find-project.sh
@@ -41,6 +41,15 @@ elif [[ $# == 1 && $1 == 'rmi' ]]; then
   grep -vx $selected $config_file > temp && mv temp $config_file 
 
   echo "Removed \"$selected\" from favorites"
+elif [[ $# == 1 && $1 == 'clean' ]]; then
+  assert_file_exists $config_dir $config_file
+
+  cat $config_file | while read line
+  do
+    if ! [[ -d $line ]]; then
+      grep -vx $line $config_file > temp && mv temp $config_file
+    fi
+  done
 else
   echo "Invalid arguments. You should either call this script with no arguments to open the projects finder, or call it with \"add\" or \"remove\" arguments"
 fi
