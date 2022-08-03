c557a9855034ff14dec27793a90e7bf9ba6d8b15
Bruno Mello <bruno.barros.mello@gmail.com>
2022-08-03 20:07:27 -0300

    add cheatsheet script

---
+++ zsh_scripts/cht.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+languages=`echo "golang lua typescript python" | tr ' ' '\n'`
+core_utils=`echo "xargs find fd mv sed awk" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$core_utils" | fzf`
+
+read -p "query: " query
+
+if [[ $languages =~ $selected ]]; then
+  curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done
+else
+  tmux neww zsh -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+fi
+
+
+
