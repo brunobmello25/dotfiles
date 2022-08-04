49a70cad61fa89757828c713968c0d1e1bebf405
Bruno Mello <bruno.barros.mello@gmail.com>
2022-08-04 15:59:55 -0300

    updates on cht.sh

--- zsh_scripts/cht.sh
+++ zsh_scripts/cht.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 languages=`echo "golang lua typescript python" | tr ' ' '\n'`
-core_utils=`echo "xargs find fd mv sed awk" | tr ' ' '\n'`
+core_utils=`echo "xargs find mv sed awk" | tr ' ' '\n'`
 
 selected=`printf "$languages\n$core_utils" | fzf`
 
@@ -9,8 +9,6 @@ read -p "query: " query
 if [[ $languages =~ $selected ]]; then
   curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done
 else
-  tmux neww zsh -c "curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+  curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done
 fi
-
-
 
