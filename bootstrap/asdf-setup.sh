a51874852e412f2b788a0e9cb86f44ee5c164056
Bruno Mello <bruno.barros.mello@gmail.com>
2023-02-23 20:04:02 -0300

    feat: add rust to asdf setup script

--- bootstrap/asdf-setup.sh
+++ bootstrap/asdf-setup.sh
@@ -1,4 +1,5 @@
 print_step_header "Installing asdf plugins"
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
 wait_for_keypress
