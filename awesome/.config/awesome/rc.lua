9a112d9931bbd4a36cbb46f7aae1a653a33a2c56
Bruno Mello <bruno.barros.mello@gmail.com>
2023-08-23 13:40:31 -0300

    ulauncher floating rule

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -445,6 +445,8 @@ awful.rules.rules = {
         "gnome-calculator",
         "pavucontrol",
         "Pavucontrol",
+        "ulauncher",
+        "Ulauncher",
       },
 
       -- Note that the name property shown in xprop might be set slightly after creation of the client
