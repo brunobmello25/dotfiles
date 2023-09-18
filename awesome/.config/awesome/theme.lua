3ec29f79fc07fd70eb68d0d0132b2161d85357b7
Bruno Mello <bruno.barros.mello@gmail.com>
2023-09-18 16:16:09 -0300

    reduce gap

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -26,7 +26,7 @@ theme.fg_focus                                  = "#ffffff"
 theme.fg_urgent                                 = "#ffffff"
 theme.fg_minimize                               = "#ffffff"
 
-theme.useless_gap                               = dpi(10)
+theme.useless_gap                               = dpi(6)
 theme.border_width                              = dpi(1)
 theme.border_normal                             = "#000000"
 theme.border_focus                              = "#dd7373"
