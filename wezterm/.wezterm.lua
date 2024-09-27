14132704c875095cf819ad1659cb3d84dcc358a2
Bruno Mello <bruno.barros.mello@gmail.com>
2024-09-27 09:33:03 -0300

    wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,28 +1,18 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
-
-config.colors = {
-  cursor_bg = '#ccc',
-  background = '#070707',
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.enable_tab_bar = false
-
--- set font to FiraCode Nerd Font
-config.font = wezterm.font_with_fallback {
-  'FiraCode Nerd Font',
-}
 
 return config
