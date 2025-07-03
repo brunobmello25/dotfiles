5eee80ce46fd8b47e6ea5a069c2b7f141d2572e5
Bruno Mello <bruno.barros.mello@gmail.com>
2025-07-03 11:01:11 -0300

    adjust gap

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -25,7 +25,7 @@ theme.fg_focus = "#ffffff"
 theme.fg_urgent = "#ffffff"
 theme.fg_minimize = "#ffffff"
 
-theme.useless_gap = dpi(6)
+theme.useless_gap = dpi(4)
 theme.border_width = dpi(1)
 theme.border_normal = "#000000"
 theme.border_focus = "#dd7373"
