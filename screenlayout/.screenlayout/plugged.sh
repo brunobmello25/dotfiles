b4d0b773ae9fa393dcc670dfeb14bb796ea11743
Bruno Mello <bruno.barros.mello@gmail.com>
2025-08-08 18:30:03 -0300

    adjust display scripts

--- screenlayout/.screenlayout/plugged.sh
+++ screenlayout/.screenlayout/plugged.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1200 --pos 0x1080 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate inverted --output DP-4 --off
+xrandr --output eDP-1 --mode 1920x1200 --pos 0x1080 --rotate normal --scale 0.67x0.67 --output HDMI-1 --primary --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate inverted --output DP-4 --off
