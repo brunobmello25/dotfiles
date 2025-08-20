c97cd8c4e8158715ca49887fbba3118ea261135f
Bruno Mello <bruno.barros.mello@gmail.com>
2025-08-19 21:29:23 -0300

    switch to chrome

--- awesome/.config/awesome/keybindings.lua
+++ awesome/.config/awesome/keybindings.lua
@@ -89,8 +89,8 @@ function keybindings.setup(terminal, modkey)
 		end, { description = "open a terminal", group = "launcher" }),
 
 		awful.key({ modkey }, "b", function()
-			awful.spawn("firefox")
-		end, { description = "spawn firefox", group = "launcher" }),
+			awful.spawn("google-chrome-stable")
+		end, { description = "spawn browser", group = "launcher" }),
 
 		awful.key({ modkey }, "space", function()
 			awful.spawn("rofi -show drun")
