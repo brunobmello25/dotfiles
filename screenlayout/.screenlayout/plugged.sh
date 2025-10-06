c1f045da3d742b042c364af68054e06d5e96f49b
Bruno Mello <bruno.barros.mello@gmail.com>
2025-10-06 14:31:00 -0300

    add plugged.sh script scale

--- screenlayout/.screenlayout/plugged.sh
+++ screenlayout/.screenlayout/plugged.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1200 --pos 0x1178 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate inverted --output DP-4 --off
+xrandr --output eDP-1 --mode 1920x1200 --scale 0.8x0.8 --pos 0x1200 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1536x1080 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 1536x0 --rotate inverted --output DP-4 --off
