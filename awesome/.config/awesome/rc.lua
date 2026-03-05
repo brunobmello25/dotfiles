42a56845c8d7b69033a41bc294b4586a162f2409
Bruno Mello <bruno.barros.mello@gmail.com>
2026-03-05 16:46:36 -0300

    switch to alacritty

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -64,7 +64,7 @@ beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 -- terminal = "x-terminal-emulator"
-local terminal = "kitty"
+local terminal = "alacritty"
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
