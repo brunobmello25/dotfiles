56767850f6b82594aac43de898dc35895c92225f
Bruno Mello <bruno.barros.mello@gmail.com>
2026-05-26 09:30:17 -0300

    update nvim

--- nvim2/.config/nvim2/init.lua
+++ nvim2/.config/nvim2/init.lua
@@ -87,4 +87,139 @@ require("lazy").setup({
 			})
 		end,
 	},
+
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = false,
+						-- Use fd if available for better performance
+						find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+					},
+					live_grep = {
+						-- Additional args for ripgrep to search hidden files
+						additional_args = function()
+							return { "--hidden", "--glob", "!.git/*" }
+						end,
+					},
+				},
+
+				extensions = {
+					["ui-select"] = { require("telescope.themes").get_dropdown() },
+				},
+			})
+
+			-- Enable Telescope extensions if they are installed
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
+			-- See `:help telescope.builtin`
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			vim.keymap.set({ "n", "v" }, "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "[S]earch [C]ommands" })
+
+			-- Add Telescope-based LSP pickers when an LSP attaches to a buffer.
+			-- If you later switch picker plugins, this is where to update these mappings.
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("telescope-lsp-attach", { clear = true }),
+				callback = function(event)
+					local buf = event.buf
+
+					vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = buf, desc = "[G]oto [R]eferences" })
+					vim.keymap.set(
+						"n",
+						"gi",
+						builtin.lsp_implementations,
+						{ buffer = buf, desc = "[G]oto [I]mplementation" }
+					)
+					vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = buf, desc = "[G]oto [D]efinition" })
+					vim.keymap.set(
+						"n",
+						"gO",
+						builtin.lsp_document_symbols,
+						{ buffer = buf, desc = "Open Document Symbols" }
+					)
+					vim.keymap.set(
+						"n",
+						"gW",
+						builtin.lsp_dynamic_workspace_symbols,
+						{ buffer = buf, desc = "Open Workspace Symbols" }
+					)
+					vim.keymap.set(
+						"n",
+						"gT",
+						builtin.lsp_type_definitions,
+						{ buffer = buf, desc = "[G]oto [T]ype Definition" }
+					)
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>/", function()
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "[/] Fuzzily search in current buffer" })
+			vim.keymap.set("n", "<leader>f/", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[S]earch [/] in Open Files" })
+
+			vim.keymap.set("n", "<leader>fn", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "[S]earch [N]eovim files" })
+		end,
+	},
+
+	{
+		"cbochs/grapple.nvim",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+		},
+		config = function()
+			vim.keymap.set("n", "<leader>ha", require("grapple").tag)
+			vim.keymap.set("n", "<leader>hh", require("grapple").toggle_tags)
+
+			vim.keymap.set("n", "<leader>1", "<cmd>Grapple select index=1<cr>")
+			vim.keymap.set("n", "<leader>2", "<cmd>Grapple select index=2<cr>")
+			vim.keymap.set("n", "<leader>3", "<cmd>Grapple select index=3<cr>")
+			vim.keymap.set("n", "<leader>4", "<cmd>Grapple select index=4<cr>")
+			vim.keymap.set("n", "<leader>5", "<cmd>Grapple select index=5<cr>")
+			vim.keymap.set("n", "<leader>6", "<cmd>Grapple select index=6<cr>")
+			vim.keymap.set("n", "<leader>7", "<cmd>Grapple select index=7<cr>")
+			vim.keymap.set("n", "<leader>8", "<cmd>Grapple select index=8<cr>")
+			vim.keymap.set("n", "<leader>9", "<cmd>Grapple select index=9<cr>")
+		end,
+	},
+
+	{
+		"bluz71/vim-moonfly-colors",
+		init = function()
+			vim.g.moonflyWinSeparator = 2
+			vim.g.moonflyTransparent = true
+
+			vim.cmd("colorscheme moonfly")
+
+			vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#20303b" })
+			vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#37222c" })
+			vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1f2231" })
+			vim.api.nvim_set_hl(0, "DiffText", { bg = "#394b70" })
+		end,
+	},
 })
